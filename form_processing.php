d420323e87cf35f0cdedf5507ba15be5658a6d2e
Kaido Soodla <kaidogta3@hotmail.com>
2014-10-25 18:57:03 +0300

    Final asdfg

--- form_processing.php
+++ form_processing.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,31 +7,46 @@
 <body>
 <pre>
 <?php 
-//$username = $_POST["username"];
-//$password = $_POST["password"];
-	
-//print_r($_POST);
-//echo "Tere " . $username . "! Teie parool on" . $password;
-
- ?>
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+   	$submit = $_POST['submit']; 
 
 
- <?php
-  
-
-			
-			
-	
-if (isset($_POST["password"])) 
-
-		$username = isset($_POST["username"]) ? $_POST["username"]) : "Puudub";
-
-  if ($username == "puudub" || $username == "" || $password == "puudub" ||  $password == "") {
-
-  		echo "Kasutajanimi või parool on puudu.";
-  	} else {
-		echo "Tere {$username}!" . "Teie parool on {$password}.";
+    /*echo "<pre>";
+      print_r($_POST);?> 
+      <?php echo "Tere {$username}! Teie parool on {$password}" ;?> 
+		?>
+<?php 
+  if (isset($_POST["username"])) {
+    $username = $_POST["username"];
+  } else {
+    $username = "puudub";
   }
+		?>
+<?php
+
+  if (isset($_POST["password"])) {
+    $password = $_POST["password"];
+  } else {
+    $password = "puudub";
+  }
+  */ 
+?>
+<?php $username = isset($_POST['username']) ? ($_POST['username']) : "puudub"; ?>
+<?php $username = isset($_POST['password']) ? ($_POST['password']) : "puudub"; ?>
+<!-- if username and parool == username/parool echo stuff
+-->
+
+<?php 
+
+
+
+  if ($username = $_POST["username"] && $password = $_POST["password"]) {
+  	
+		echo "Tere {$username}! Teie parool on {$password}";} 
+			else {echo "Kasutajanimi või parool on puudu.";};
+		
+
 ?>
  </pre>
 </body>
--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>johvits</title>
+	<title>pystijalu</title>
 </head>
 <body>
 
-<! -- action="" atribuudi väärtuseks on dokument, kuhu pärast vormi postitamise nupule vajutamist suunatakse (kuhu vormis paiknev info postitatakse).
 
-method="", mille vääruseks on post.
-Määrab ära, mis tüüpi päring teostatakse vormi postitamise nupule vajutamisel.-->
 
 <form action="form_processing.php" method="post">
+
 	Kasutaja nimi: <input type="text" name="username"></input>
+
 	Password: <input type="password" name="password"></input>
-	<input type="submit" value="Saada"></input>
+
+	<input type="submit" value="Saada" name="submit"></input>
+
+
 </form>
 
 
